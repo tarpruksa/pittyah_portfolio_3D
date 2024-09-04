028d969162d62bc37dc2f249aa6f41c050fcc32f
Pittayah <tar.violin@gmail.com>
2024-09-04 21:27:28 +0700

    Add vertical time line in work section.

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,50 +1,96 @@
-import React from 'react'
-import { AnimationType, NavState, getNavString } from '../constants/type'
+import { ExperienceData, NavState } from '../constants/type'
 import SectionWrapper from '../hoc/SectionWrapper'
-import { motion } from 'framer-motion'
-import { textVariant } from '../constants/utils'
-import { styles } from '../style'
-import { lex247logo, lex247 } from '../assets'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+import { Pill, Title } from './subcomponents'
+import { experiences } from '../constants/data'
+import { FaSquareArrowUpRight } from 'react-icons/fa6'
 
-const Work = () => {
+const ExperienceCard = (props: ExperienceData) => {
+  const { title, company, icon, date, link, details, stacks } = props
   return (
-    <div className="min-h-[100vh] flex flex-col items-start">
-      <div className="flex gap-10 border-blue p-8">
-        <motion.div variants={textVariant()}>
-          <p className={styles.sectionSubText}>2022 - present</p>
-          <img src={lex247} className="mt-2 h-44" />
-        </motion.div>
-
-        <div className="border-grey p-8 rounded-[10px]">
-          <div className="flex items-end gap-4 mb-4">
-            <img
-              src={lex247logo}
-              style={{ filter: 'brightness(3)', height: '45px' }}
-            />
-            <p>A True-Cloud Legal Practice Management Software</p>
-          </div>
-          <p className={styles.sectionSubText}>Frontend Developer</p>
-          <div>
-            <p>
-              Build, Improve, Develop features for lawyer firm. Although title
-              as frontend but I also do a lot of part on backend.
-            </p>
-
-            <div className="flex gap-5 mt-2">
-              <p>What I make changes:</p>
-              <div>
-                <p>
-                  New control panel for the application, 20 pages, over 300
-                  configable system included.
-                </p>
-                <p>3 new features: Will Management, Time Bank, Time Capture</p>
-              </div>
-            </div>
-          </div>
+    <VerticalTimelineElement
+      contentStyle={{
+        background: '#0f172a',
+        color: '#cbd5e1',
+        boxShadow: '2px 4px 4px #000',
+      }}
+      contentArrowStyle={{ borderRight: '7px solid #0f172a' }}
+      date={date}
+      dateClassName="tracking-wider"
+      iconStyle={{
+        overflow: 'hidden',
+        boxShadow: '0 0px 20px #dadada',
+      }}
+      icon={
+        <div className="flex justify-center items-center w-full h-full metalic">
+          <img
+            src={icon}
+            alt={company}
+            className="w-[60%] h-[60%] object-contain"
+          />
         </div>
+      }
+    >
+      <div>
+        <h3 className="text-slate-300 text-xl md:text-2xl font-bold">
+          {title}
+        </h3>
+        <p className="section-subheader" style={{ margin: 0 }}>
+          {company}
+        </p>
       </div>
-    </div>
+      {link && (
+        <a
+          href={link}
+          target="_blank"
+          className="absolute top-6 2xl:top-7 right-6"
+        >
+          <FaSquareArrowUpRight size={20} className="link-icon" />
+        </a>
+      )}
+      <ul className="mt-5 list-disc ml-5 space-y-2">
+        {details.map((detail, index) => {
+          const isSubList = detail.startsWith('-')
+          return (
+            <li
+              key={`experience-point-${index}`}
+              className={`text-slate-400 text-[0.8rem] ${
+                isSubList ? 'ml-5' : ''
+              }`}
+            >
+              {isSubList ? detail.substring(1) : detail}
+            </li>
+          )
+        })}
+      </ul>
+      {stacks && (
+        <div className="flex gap-2 flex-wrap mt-5 mb-2">
+          {stacks.map((stack, index) => (
+            <Pill key={index} text={stack} index={index} />
+          ))}
+        </div>
+      )}
+    </VerticalTimelineElement>
   )
 }
 
-export default SectionWrapper(Work, AnimationType.Left)
+const Work = () => {
+  return (
+    <>
+      <Title text="Work Experiences" />
+      <div className="mt-10 flex flex-col">
+        <VerticalTimeline lineColor="#888888" className="!w-full !max-w-none">
+          {experiences.map((data, index) => (
+            <ExperienceCard key={`experience-${index}`} {...data} />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(Work, NavState.Experience)
